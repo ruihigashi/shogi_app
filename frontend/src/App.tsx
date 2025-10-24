8f6652f76396a0489409633d734e57220d672cb0
rui0421 <rui.04215@icloud.com>
2025-10-24 18:53:34 +0900

    style: MainPageの画面レイアウト& root: 各画面のルート設定

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
-export function App() {
+import { Routes, Route } from "react-router"
+import { SettingPage } from "./pages/SettingPage"
+import { ShogiBattlePage } from "./pages/ShogiBattlePage"
+import { MainPage } from "./pages/MainPage"
 
+export function App() {
   return (
-    <>
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/setting" element={<SettingPage />} />
+        <Route path="/battle" element={<ShogiBattlePage />}/>
+      </Routes>
+    </div>
   )
 }
---
+++ frontend/src/pages/SettingPage.tsx
@@ -0,0 +1,9 @@
+import { StartButton } from "../components/Button/Buttons/StartButton";
+
+export function SettingPage() {
+    return (
+        <div>
+         <StartButton />
+        </div>
+    )
+}
