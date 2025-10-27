8087a6760c7f9d75c8ba05e0a74cdd15345ce679
rui0421 <rui.04215@icloud.com>
2025-10-27 15:19:12 +0900

    refactor: ルーティングの修正

--- frontend/src/pages/ShogiBattlePage.tsx
+++ frontend/src/pages/ShogiBattlePage.tsx
@@ -1,7 +1,10 @@
+import { BackGround } from "../components/BackGround";
+import { BackPageButton } from "../components/Buttons/BackPageButton";
+
 export function ShogiBattlePage() {
     return (
         <div>
-
+            <BackGround title="いざ尋常に勝負" backPage={<BackPageButton />}/>
         </div>
     )
 }
